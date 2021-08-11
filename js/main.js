469df79a0100ff1fdc07e293e5a328aeb776a7af
charlie-jyj <62326489+charlie-jyj@users.noreply.github.com>
2021-08-11 18:06:08 +0900

    BEM

--- js/main.js
+++ js/main.js
@@ -14,3 +14,10 @@ searchInputEl.addEventListener('blur', function () {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+
+const badgeEl = docuement.querySelector("header .badges");
+
+window.addEventListener('scroll', function () {
+    
+});
