40124e00b31befa5d38e389029536d0065a1f453
charlie-jyj <62326489+charlie-jyj@users.noreply.github.com>
2021-08-18 18:04:51 +0900

    visual end

--- js/main.js
+++ js/main.js
@@ -16,8 +16,35 @@ searchInputEl.addEventListener('blur', function () {
 });
 
 
-const badgeEl = docuement.querySelector("header .badges");
+const badgeEl = document.querySelector("header .badges");
 
-window.addEventListener('scroll', function () {
-    
+// _.throttle(func, wait)
+// wait 시간 동안 한 번만 실행된다
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+        // gsap
+        gsap.to(badgeEl, {
+            opacity: 0,
+            display: "none",
+            duration: .5,
+        });
+    } else {
+        gsap.to(badgeEl, {
+            opacity: 1,
+            display: "block",
+            duration: .5,
+        });
+    }
+}, 300));
+
+
+//fade-in
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+    gsap.to(fadeEl, {
+        duration: 1,
+        opacity: 1,
+        delay: (index+1)*.7,
+    })
 });
